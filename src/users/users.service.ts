88c4c7e72465c4035285913c7c5dc88381008037
DagaPawlos <kk.dagmara@gmail.com>
2023-02-18 17:23:29 +0100

    Validate user uniquelogin

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RegisterUserDto } from 'src/auth/dto/register-user.dto';
 import { Repository } from 'typeorm';
@@ -7,15 +7,22 @@ import { User } from './users.model';
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(User) private mealsRepository: Repository<User>,
+    @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
-  
+
   async addUser(body: RegisterUserDto): Promise<User> {
+    const userExist = await this.usersRepository.findOne({
+      where: { login: body.login },
+    });
+    if (userExist) {
+      throw new UnauthorizedException();
+    }
+
     const newUser = new User();
     newUser.login = body.login;
     newUser.password = body.password;
 
-    const user = await this.mealsRepository.save(newUser);
+    const user = await this.usersRepository.save(newUser);
     return user;
   }
 }
