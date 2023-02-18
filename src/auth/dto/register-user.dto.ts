0ca7b790fdb63822e3437e5432ab55c3a3cb2894
DagaPawlos <kk.dagmara@gmail.com>
2023-02-18 18:08:17 +0100

    password validation

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,9 +1,13 @@
-import { IsString } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class RegisterUserDto {
   @IsString()
   login: string;
 
   @IsString()
+  @MinLength(4)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   password: string;
 }
